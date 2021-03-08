5c474655e5a282e8c1468887053b15ddbdb3f767
aponte411 <aponted511@gmail.com>
2021-03-07 21:05:35 -0500

    feat: min total distance

--- graphs/min_total_distance.py
+++ graphs/min_total_distance.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from collections import namedtuple, deque
 from typing import List
 
 Point = namedtuple('Point', ('row', 'col'))
@@ -26,6 +26,11 @@ def calculate_manhattan_distance(
 
 
 def min_total_distance(grid: List[List[int]]) -> int:
+    """
+    Time: O(m^2 * n^2)
+    Space: O(m^2, n^2)
+    For each node in the grid we calculate the manhattan distance.
+    """
     points = get_points(grid)
     # if you use float('-inf') you get funny errors
     min_distance = float('inf') # + infinity upper bound
@@ -39,11 +44,45 @@ def min_total_distance(grid: List[List[int]]) -> int:
             min_distance = min(min_distance, distance)
     return min_distance
 
+Node = namedtuple('Node', ('x', 'y', 'dist'))
+
+
+def min_total_distance_bfs(self, grid: List[List[int]]) -> int:
+    def bfs(x: int, y: int) -> int:
+        total_distance = 0
+        # create set
+        visited = [[False]*n for _ in range(m)]
+        que = deque([Node(x, y, 0)])
+        while que:
+            node = que.popleft()
+            if not (0 <= node.x < m and 0 <= node.y < n) or visited[node.x][node.y]:
+                continue
+            # If we find a home
+            if grid[node.x][node.y] == 1:
+                total_distance += node.dist
+            visited[node.x][node.y] = True
+            que.append(Node(node.x + 1, node.y, node.dist+1))
+            que.append(Node(node.x - 1, node.y, node.dist+1))
+            que.append(Node(node.x, node.y+1, node.dist+1))
+            que.append(Node(node.x, node.y-1, node.dist+1))
+
+        return total_distance
+
+    m = len(grid)
+    n = len(grid[0])
+    min_distance = float('inf')
+    for row in range(m):
+        for col in range(n):
+            distance = bfs(row, col)
+            min_distance = min(min_distance, distance)
+    return min_distance
+
 def test():
     grid = [[1,0,0,0,1],[0,0,0,0,0],[0,0,1,0,0]]
     expected = 6
     result = min_total_distance(grid)
     print(result)
+    assert result == expected
 
 if __name__ == "__main__":
     test()
