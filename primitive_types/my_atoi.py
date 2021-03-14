1ca2be5833f75631dca1af4dfd68060184eba707
aponte411 <aponted511@gmail.com>
2021-03-14 10:25:24 -0400

    feat: fix my atoi

--- primitive_types/my_atoi.py
+++ primitive_types/my_atoi.py
@@ -1,9 +1,13 @@
+import pytest
+import string
+
 MAX_INT = 2**31
 
-def my_atoi(string: str) -> int:
+def my_atoi(s: str) -> int:
     if string == "":
         return 0
-    new_string = string.strip()
+    # remove white space
+    new_string = s.strip()
     sign = 1
     if new_string and new_string[0] == '-':
         sign = -1
@@ -12,23 +16,27 @@ def my_atoi(string: str) -> int:
         new_string = new_string[1:]
 
     number = 0
-    for char in string:
+    for char in new_string:
         if char.isdigit():
-            number = (number*10)+int(char)
+            # multiply the number by the base (10) and convert
+            # to integer.
+            number = (number*10) + string.hexdigits.index(char)
         else:
+            # if not a valid digit, break out
             break
     result = (number*sign)
     if result >= MAX_INT:
+        # less than max
         return MAX_INT - 1
     elif result < - MAX_INT:
         return - MAX_INT
     return result
 
-def test():
-    string = "   -42"
-    expected = -42
-    result = my_atoi(string)
-    print(result)
 
-if __name__ == "__main__":
-    test()
+def test_negative():
+    s = "   -42"
+    expected = -42
+    result = my_atoi(s)
+    assert result == expected
+
+pytest.main()
