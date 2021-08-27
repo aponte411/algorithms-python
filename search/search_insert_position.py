e90ff069fafb0be50dfd1f35618ebcb871077fc0
aponte411 <aponted511@gmail.com>
2021-08-27 15:34:24 -0400

    feat: add search insert position

---
+++ search/search_insert_position.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+def search_insert(nums: List[int], target: int) -> List[int]:
+    """O(n log n) time O(1)"""
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        pivot = left + ((right - left)//2)
+        if nums[pivot] == target:
+            return pivot
+        elif nums[pivot] > target:
+            right = pivot
+        else:
+            left = pivot + 1
+    return left
+
+def main():
+    nums = [1,3,5,6]
+    target = 5
+    result = search_insert(nums, target)
+    expected = 2
+    assert result == expected
+    print(result)
+
+if __name__ == '__main__':
+    main()
