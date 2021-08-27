96944661a0b945abde54423fd0bd3e534eb19343
aponte411 <aponted511@gmail.com>
2021-08-27 15:19:51 -0400

    feat: added some more algos

---
+++ search/first_bad_version.py
@@ -0,0 +1,19 @@
+"""You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API."""
+
+def is_bad_version(version int) -> bool:
+    """this function is assumed to be implemented"""
+    return True
+
+def first_bad_version(n: int) -> int:
+    left, right = 1, n
+    while left < right:
+        pivot = left + ((right - left)//2)
+        if is_bad_version(pivot):
+            right = pivot
+        else:
+            left = pivot + 1
+    return left
